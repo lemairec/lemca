a01edb7637d1bdee74b694d115d84de3d0d2e001
Clement Lemaire <lemairec02@gmail.com>
2020-09-17 17:37:25 +0200

    Fix

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
 cp ~/lemca/install/lemca.desktop ~/Desktop
-sudo chmod +x ~/Desktop/agtf.desktop
+sudo chmod +x ~/Desktop/lemca.desktop
 mkdir -p ~/.config/autostart
 cp ~/lemca/install/lemca_autostart.desktop ~/.config/autostart
