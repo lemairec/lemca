768abbdb46bae470bbd9e730f487e0f5506b8f5b
Clement Lemaire <lemairec02@gmail.com>
2020-10-11 10:12:14 +0200

    Fix logo

--- lemca.py
+++ lemca.py
@@ -89,7 +89,7 @@ y2 = 0.6
 #btn.grid(column=1, row=0)
 
 
-img = PIL.Image.open("~/lemca/gui/logo.png")
+img = PIL.Image.open("gui/logo.png")
 img = img.resize((400, 400))
 img = PIL.ImageTk.PhotoImage(img)
 
@@ -100,28 +100,28 @@ btn.place(relx = 0.5, rely = 0.3, anchor = 'center')
 
 
 
-image1 = PIL.Image.open("~/lemca/gui/bineuse.png")
+image1 = PIL.Image.open("gui/bineuse.png")
 image1 = image1.resize((size, size))
 image1 = PIL.ImageTk.PhotoImage(image1)
 
 btn = Button(window, image=image1, command=clicked_bineuse, relief=FLAT, highlightthickness=0, bd=0)
 btn.place(relx = 0.2, rely = y1, anchor = 'center')
 
-image2 = PIL.Image.open("~/lemca/gui/refresh.png")
+image2 = PIL.Image.open("gui/refresh.png")
 image2 = image2.resize((size, size))
 image2 = PIL.ImageTk.PhotoImage(image2)
 
 btn = Button(window, image=image2, command=install_bineuse, relief=FLAT, highlightthickness=0, bd=0)
 btn.place(relx = 0.4, rely = y1, anchor = 'center')
 
-image3 = PIL.Image.open("~/lemca/gui/reseau.png")
+image3 = PIL.Image.open("gui/reseau.png")
 image3 = image3.resize((size, size))
 image3 = PIL.ImageTk.PhotoImage(image3)
 
 btn = Button(window, image=image3, command=teamviewer, relief=FLAT, highlightthickness=0, bd=0)
 btn.place(relx = 0.6, rely = y1, anchor = 'center')
 
-image4 = PIL.Image.open("~/lemca/gui/off.png")
+image4 = PIL.Image.open("gui/off.png")
 image4 = image4.resize((size, size))
 image4 = PIL.ImageTk.PhotoImage(image4)
 
@@ -130,21 +130,21 @@ btn.place(relx = 0.8, rely = y1, anchor = 'center')
 
 
 if config.gps : 
-    image5 = PIL.Image.open("~/lemca/gui/gps.png")
+    image5 = PIL.Image.open("gui/gps.png")
     image5 = image5.resize((size2, size2))
     image5 = PIL.ImageTk.PhotoImage(image5)
 
     btn = Button(window, image=image5, command=clicked_gps, relief=FLAT, highlightthickness=0, bd=0)
     btn.place(relx = 0.2, rely = y2, anchor = 'center')
 
-    image6 = PIL.Image.open("~/lemca/gui/refresh.png")
+    image6 = PIL.Image.open("gui/refresh.png")
     image6 = image6.resize((size2, size2))
     image6 = PIL.ImageTk.PhotoImage(image6)
 
     btn = Button(window, image=image6, command=install_gps, relief=FLAT, highlightthickness=0, bd=0)
     btn.place(relx = 0.4, rely = y2, anchor = 'center')
 
-    image9 = PIL.Image.open("~/lemca/gui/refresh.png")
+    image9 = PIL.Image.open("gui/refresh.png")
     image9 = image9.resize((size2, size2))
     image9 = PIL.ImageTk.PhotoImage(image9)
 
