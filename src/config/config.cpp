5838ff5fe2623be4e57a20b2f8f1c7a342453380
lemaire <lemaire@abvent.fr>
2022-03-20 10:21:24 +0100

    Fix

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -19,7 +19,7 @@ Config::Config(){
     m_robot_inrows_run = "~/inrows/build/inrows";
     m_robot_inrows_update = "cd ~/inrows && mkdir -p build && git pull && cd build && cmake .. && make -j4; read";
     m_make_archive = "cd ~/bineuse_src; git pull; sh make_archive.sh && read";
-    m_update_lemca = "cd ~/lemca && git pull && rm -rf build_new && rm -rf build_old && mkdir build_new && cd build_new && cmake .. && make -j4 && cd ~/lemca && mv build build_old && mv build_new build && read";
+    m_update_lemca = "cd ~/lemca && git pull && rm -rf build_new && rm -rf build_old && mkdir build_new && cd build_new && cmake .. && make -j4 && cd ~/lemca && mv build build_old && mv build_new build && echo 5 && sleep 1 && echo 4 && sleep 1 && echo 3 && sleep 1&& echo 2 && sleep 1 && echo 1 && sleep 1 && /sbin/shutdown -h now";
     
 }
 
