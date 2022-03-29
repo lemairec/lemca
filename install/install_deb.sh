5c9683eff3529aa9c01a1d74c94e62f03b0381d9
lemaire <lemaire@abvent.fr>
2022-03-29 21:41:21 +0200

    Fix

--- install/install_deb.sh
+++ install/install_deb.sh
@@ -5,3 +5,5 @@ apt-get install -y libboost-iostreams-dev
 apt-get install -y libboost-system-dev
 apt-get install -y libopencv-dev
 apt-get install -y wget
+apt-get install -y curl
+apt-get install -y vim
