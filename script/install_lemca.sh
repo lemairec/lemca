f4be4da3c78d0c6617057f74256a6b8dba99a1a8
lemaire <lemaire@abvent.fr>
2022-05-01 21:55:09 +0200

    Fix install

--- script/install_lemca.sh
+++ script/install_lemca.sh
@@ -6,13 +6,4 @@ mkdir ~/lemca/build
 echo "#### COMPIL";
 cd ~/lemca/build && cmake .. && make
 
-echo "#### PREPARE OPT";
-rm -rf /opt/lemca;
-
-mkdir -p /opt/lemca;
-mkdir -p /opt/lemca/bin
-mkdir -p /opt/lemca/etc
-
-mv ~/lemca/build/lemca /opt/lemca/bin/
-
 echo "#### OK";
