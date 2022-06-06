47f56cc8aa776493653d595739155f8de6be8edf
lemaire <lemaire@abvent.fr>
2022-06-06 15:02:22 +0200

    Paquet de merde a la fin

--- src/sh/install_dep.sh
+++ src/sh/install_dep.sh
@@ -11,7 +11,7 @@ apt-get install -y libopencv-dev
 apt-get install -y wget
 apt-get install -y curl
 apt-get install -y vim
-apt-get install -y florence
 apt-get install -y qtmultimedia5-dev
 apt-get install -y libqt5multimedia5-plugins
 apt-get install -y nmap
+apt-get install -y florence
