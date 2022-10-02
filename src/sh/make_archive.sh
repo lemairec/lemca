3176bfbf4d701737f7c2afc63d51582f5a99bc0c
lemaire <lemaire@abvent.fr>
2022-10-02 23:09:04 +0200

    Make archive

--- src/sh/make_archive.sh
+++ src/sh/make_archive.sh
@@ -3,7 +3,8 @@
 archive=~/bineuse_src/archive/bineuse
 
 rm -rf ~/bineuse
-rm -rf ~/bineuse_src/build
+rm -rf ~/bineuse_src
+git clone git@github.com:lemairec/bineuse.git ~/bineuse_src
 rm -rf $archive
 rm -rf ~/bineuse_src/archive
 
@@ -22,3 +23,5 @@ tar -czvf bineuse.tar.gz bineuse
 cd ~/;
 
 curl --request POST 'https://maplaine.fr/lemca/send_file' --form 'myfile=@"./bineuse_src/archive/bineuse.tar.gz"'
+
+rm -rf ~/bineuse_src
