79f6b5b55143db6bb8a3b2a0b7a37462ac69c30d
lemairec <lemairec02@gmail.com>
2023-01-22 13:46:09 +0100

    lemca/bineuse_binary?branch=

--- src/sh/bineuse_update_wifi.sh
+++ src/sh/bineuse_update_wifi.sh
@@ -5,7 +5,8 @@ echo "--- branche $branch\n"
 
 rm -rf ~/bineuse.tar.gz
 echo  "**** telechargement"
-wget -c https://maplaine.fr/lemca/bineuse_$branch.tar.gz -O ~/bineuse.tar.gz
+
+wget -c https://maplaine.fr/lemca/bineuse_binary?branch=$branch -O ~/bineuse.tar.gz
 
 echo  "**** telechargement ok"
 rm -rf bineuse
