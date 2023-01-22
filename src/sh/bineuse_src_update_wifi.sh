a3ef0c346c3557acd13b7058c91d7cbff744b396
lemairec <lemairec02@gmail.com>
2023-01-22 12:27:26 +0100

    Try

--- src/sh/bineuse_src_update_wifi.sh
+++ src/sh/bineuse_src_update_wifi.sh
@@ -25,8 +25,3 @@ cp -r ~/bineuse_src/gui $archive
 cp -r ~/bineuse_src/sound $archive
 
 mkdir -p $archive/images
-cp -r ~/bineuse_src/images/colza.jpg $archive/images/colza.jpg
-cp -r ~/bineuse_src/images/ble.jpg $archive/images/ble.jpg
-cp -r ~/bineuse_src/images/mais.jpg $archive/images/mais.jpg
-cp -r ~/bineuse_src/images/betterave_sale.jpg $archive/images/betterave_sale.jpg
-cp -r ~/bineuse_src/images/quinoa_sale.jpg $archive/images/quinoa_sale.jpg
