731987a6d30823b0e4691cf64dce2bfaf1160b77
lemairec <lemairec02@gmail.com>
2023-01-22 13:02:19 +0100

    Sound & image

--- src/sh/bineuse_src_update_wifi.sh
+++ src/sh/bineuse_src_update_wifi.sh
@@ -23,6 +23,13 @@ mkdir ~/bineuse
 archive=~/bineuse
 cp ~/bineuse_src/build/bineuse $archive
 cp -r ~/bineuse_src/gui $archive
-cp -r ~/bineuse_src/sound $archive 2>/dev/null || :
+cp -r ~/bineuse_src/sound $archive
+
+mkdir -p $archive/images
+cp -r ~/bineuse_src/images/colza.jpg $archive/images/colza.jpg
+cp -r ~/bineuse_src/images/ble.jpg $archive/images/ble.jpg
+cp -r ~/bineuse_src/images/mais.jpg $archive/images/mais.jpg
+cp -r ~/bineuse_src/images/betterave_sale.jpg $archive/images/betterave_sale.jpg
+cp -r ~/bineuse_src/images/quinoa_sale.jpg $archive/images/quinoa_sale.jpg
 
 mkdir -p $archive/images
