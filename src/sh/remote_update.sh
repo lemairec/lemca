1ef52286c0fa01126351328e8a31146bc6513220
lemairec <lemairec02@gmail.com>
2023-05-12 10:37:19 +0200

    Remote update

--- src/sh/remote_update.sh
+++ src/sh/remote_update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 spawn ssh-copy-id -o StrictHostKeyChecking=no 5chmlLEM1cale26@remote.lemcavision.com
+expect "password:" { send "test\n" }
 
-expect "password:"
-send "test\n"
-expect eof
+spawn ssh 5chmlLEM1cale26@remote.lemcavision.com "sh new_connection.sh"
+expect "password:" { send "test\n" }
