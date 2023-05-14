95d1d9add5ca91a163ebcc1064b642248293956d
lemairec <lemairec02@gmail.com>
2023-05-14 16:49:47 +0200

    Fix?

--- src/sh/remote_update.sh
+++ src/sh/remote_update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-
 spawn ssh-copy-id -o StrictHostKeyChecking=no 5chmlLEM1cale26@remote.lemcavision.com
-expect "password:" { send "test\n" }
-
+expect "password:"
+send "test\n"
+expect eof
