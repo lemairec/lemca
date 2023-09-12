b87fbfb5f86ad4cf05f8045c2b670dcf0c92750f
lemairec <lemairec02@gmail.com>
2023-09-12 21:16:51 +0200

    v_23_03 bineuse

--- src/gui/option_widget.cpp
+++ src/gui/option_widget.cpp
@@ -557,7 +557,7 @@ void OptionWidget::setSizePage4(){
     m_version_selected.clear();
     m_version_selected.addValue(("test"));
     m_version_selected.addValue(("prod"));
-    m_version_selected.addValue(("v_22_09"));
+    m_version_selected.addValue(("v_23_03"));
     m_version_selected.addValue(("v_22_03"));
 };
 
