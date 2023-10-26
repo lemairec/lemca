20b3502d9b28c16cee3a53d84721fc626f1ca5e4
lemairec <lemairec02@gmail.com>
2023-10-26 10:54:09 +0200

    Make archive & binary gps

---
+++ src/sh/lemca_gps_make_archive.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+archive=~/lemca_gps_src/archive/bineuse
+
+branch=$1;
+echo "--- branche $branch\n"
+
+rm -rf ~/lemca_gps
+rm -rf ~/lemca_gps_src
+git clone git@github.com:lemairec/lemca_gps.git ~/lemca_gps_src
+
+cd ~/lemca_gps_src
+git checkout origin/$branch
+
+rm -rf $archive
+rm -rf ~/lemca_gps_src/archive
+
+mkdir -p ~/lemca_gps_src/build && cd ~/lemca_gps_src/build  &&  cmake .. && make -j 4
+
+mkdir -p $archive
+
+cp ~/lemca_gps_src/build/bineuse $archive
+cp -r ~/lemca_gps_src/gui $archive
+cp -r ~/lemca_gps_src/sound $archive
+
+head=$(git rev-parse HEAD)
+version=$(~/lemca_gps_src/build/lemca_gps --version)
+echo $version > $archive/version.txt
+echo $branch >> $archive/version.txt
+git rev-parse HEAD >> $archive/version.txt
+
+
+cd ~/lemca_gps_src/archive
+tar -czvf lemca_gps.tar.gz lemca_gps
+cd ~/;
+curl --request POST "https://cloud.lemcavision.com/binary/send_lemca_gps?branch=$branch&head=$head&version=$version" --form 'myfile=@"./bineuse_src/archive/bineuse.tar.gz"'
+
+rm -rf ~/lemca_gps_src
---
+++ src/sh/lemca_gps_update_wifi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+branch=$1;
+constructeur=$2;
+panel=$3;
+echo "--- branche $branch\n"
+echo "--- constructeur $constructeur\n"
+echo "--- panel $panel\n"
+
+rm -rf ~/lemca_gps.tar.gz
+echo  "**** telechargement"
+echo "https://cloud.lemcavision.com/binary/lemca_gps?branch=$branch&constructeur=$constructeur&panel=$panel\n"
+
+wget -c https://cloud.lemcavision.com/binary/lemca_gps?branch=$branch\&constructeur=$constructeur\&panel=$panel -O ~/bineuse.tar.gz
+
+echo  "**** telechargement ok"
+rm -rf lemca_gps
+tar -xzvf lemca_gps.tar.gz
+
