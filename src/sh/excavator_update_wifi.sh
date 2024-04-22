603d8dc11b00cc352c7fa860af3915faeacb404f
lemairec <lemairec02@gmail.com>
2024-04-22 12:23:13 +0200

    Fix

--- src/sh/excavator_update_wifi.sh
+++ src/sh/excavator_update_wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-[ ! -d ~/lemca_excavator ] && git clone https://github.com/lemairec/lemca_excavator.git ~/lemca_excavator
+[ ! -d ~/lemca_excavator ] && git clone git@github.com:lemairec/lemca_excavator.git ~/lemca_excavator
 cd ~/lemca_excavator
 mkdir -p build
 git pull
