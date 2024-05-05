5f8fd45ba0f711a2e0e5fd640129a1d803f9da5d
lemairec <lemairec02@gmail.com>
2024-05-05 20:36:18 +0200

    Fix

--- src/cameras_modules.cpp
+++ src/cameras_modules.cpp
@@ -14,9 +14,9 @@ CamerasModules::CamerasModules(){
     m_url_cam1_2 = "http://192.168.1.30/cgi-bin/control/serverinfo3.cgi?id=admin&passwd=admin&action=getsysinfo3";
     m_url_cam1_3 = "http://192.168.1.30/cgi-bin/control/camwdr.cgi?id=admin&passwd=admin&action=getdnr&stream=all";
     
-    m_url_cam1_1 = "http://192.168.1.31/cgi-bin/control/connectinfo.cgi?id=admin&passwd=admin&action=getconnect&stream=all";
-    m_url_cam1_2 = "http://192.168.1.31/cgi-bin/control/serverinfo3.cgi?id=admin&passwd=admin&action=getsysinfo3";
-    m_url_cam1_3 = "http://192.168.1.31/cgi-bin/control/camwdr.cgi?id=admin&passwd=admin&action=getdnr&stream=all";
+    m_url_cam2_1 = "http://192.168.1.31/cgi-bin/control/connectinfo.cgi?id=admin&passwd=admin&action=getconnect&stream=all";
+    m_url_cam2_2 = "http://192.168.1.31/cgi-bin/control/serverinfo3.cgi?id=admin&passwd=admin&action=getsysinfo3";
+    m_url_cam2_3 = "http://192.168.1.31/cgi-bin/control/camwdr.cgi?id=admin&passwd=admin&action=getdnr&stream=all";
     
 }
 
@@ -27,12 +27,16 @@ void CamerasModules::refresCam1(){
     m_cam1.clear();
     MyQTNetwork * m_qt_network = MyQTNetwork::Instance_ptr();
     m_qt_network->callUrl(m_url_cam1_1);
+    m_qt_network->callUrl(m_url_cam1_2);
+    m_qt_network->callUrl(m_url_cam1_3);
 }
 
 void CamerasModules::refresCam2(){
     m_cam2.clear();
     MyQTNetwork * m_qt_network = MyQTNetwork::Instance_ptr();
     m_qt_network->callUrl(m_url_cam2_1);
+    m_qt_network->callUrl(m_url_cam2_2);
+    m_qt_network->callUrl(m_url_cam2_3);
 }
 
 void CamerasModules::handleReply(const std::string & url, const std::string &data){
