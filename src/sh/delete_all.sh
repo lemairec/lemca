b2a63a8cf89fc4495f532b684034c616b8c2beb7
lemairec <lemairec02@gmail.com>
2025-03-09 11:28:40 +0100

    Fix delete All

--- src/sh/delete_all.sh
+++ src/sh/delete_all.sh
@@ -3,6 +3,7 @@ set -e
 
 echo  "**** clean 1"
 
+rm -rf ~/agrigpspi
 rm -rf ~/lemca_data/agrigpspi
 rm -rf ~/lemca_data/inrows
 
@@ -13,7 +14,14 @@ rm -f ~/lemca_data/lemca/log.txt
 echo  "**** clean bineuse"
 rm -rf ~/lemca_data/bineuse/image_save_auto
 rm -f ~/lemca_data/bineuse/log.txt
+rm -f ~/lemca_data/bineuse/log
+rm -f ~/lemca_data/bineuse/image_save
+rm -f ~/lemca_data/bineuse/plant
 
 echo  "**** clean lemca_gps"
 rm -rf ~/lemca_data/lemca_gps/autopaths
+rm -rf ~/lemca_data/lemca_gps/job
+rm -rf ~/lemca_data/lemca_gps/line
+rm -rf ~/lemca_data/lemca_gps/parcelle
 rm -f ~/lemca_data/lemca_gps/log.txt
+rm -f ~/lemca_data/lemca_gps/tmp_cmd
