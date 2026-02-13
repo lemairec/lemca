4a1b4bc2845a3f6283f49b7babdae2294574d204
Clement Lemaire <lemairec@MacBook-Pro-de-Clement.local>
2026-02-13 13:31:52 +0100

    Fix

--- src/sh/remote_update.sh
+++ src/sh/remote_update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/expect -f
 echo lemca | su -c "sed -i 's|deb.debian.org|archive.debian.org|g' /etc/apt/sources.list"
 echo lemca | su -c "sed -i 's|security.debian.org|archive.debian.org|g' /etc/apt/sources.list"
+echo lemca | su -c "apt update"
 echo lemca | su -c "apt-get install sshpass"
 sshpass -p "test" ssh-copy-id -o StrictHostKeyChecking=no 5chmlLEM1cale26@remote.lemcavision.com
