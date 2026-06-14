7c36f7157abd3e3e390e4caf3058b26582574f73
Clement Lemaire <lemairec@macbookpro.home>
2026-06-14 23:58:39 +0200

    Fix

--- src/sh/diag_reseau.sh
+++ src/sh/diag_reseau.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-cat /sys/class/net/enp2s0/carrier
-cat /sys/class/net/enp2s0/speed
-cat /sys/class/net/enp2s0/duplex
+cat /sys/class/net/enp1s0/carrier
+cat /sys/class/net/enp1s0/speed
+cat /sys/class/net/enp1s0/duplex
