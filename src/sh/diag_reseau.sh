ecb226ecde22530cb65b6fb71bd2addab2ac787d
Clement Lemaire <lemairec@macbookpro.home>
2026-06-15 00:28:43 +0200

    Fix

--- src/sh/diag_reseau.sh
+++ src/sh/diag_reseau.sh
@@ -4,7 +4,8 @@ set -e
 cat /sys/class/net/enp1s0/carrier
 cat /sys/class/net/enp1s0/speed
 cat /sys/class/net/enp1s0/duplex
-
+echo "good value 1 100 full\n"
+echo "errors :\n"
 cat /sys/class/net/enp1s0/statistics/rx_crc_errors
 cat /sys/class/net/enp1s0/statistics/rx_errors
 
