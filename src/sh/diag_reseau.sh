9d1fd098deccbf10b109ba2c0cf666a9b2ef38d4
Clement Lemaire <lemairec@macbookpro.home>
2026-08-24 08:28:16 +0200

    Reseau

--- src/sh/diag_reseau.sh
+++ src/sh/diag_reseau.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "connexion 1 : "
 echo "carrier (good value 1) : "
 cat /sys/class/net/enp1s0/carrier
 echo "speed (good value 100) : "
@@ -10,5 +11,17 @@ cat /sys/class/net/enp1s0/duplex
 echo "errors :\n"
 cat /sys/class/net/enp1s0/statistics/rx_crc_errors
 cat /sys/class/net/enp1s0/statistics/rx_errors
+
+
+echo "connexion 2 : "
+echo "carrier (good value 1) : "
+cat /sys/class/net/enp2s0/carrier
+echo "speed (good value 100) : "
+cat /sys/class/net/enp2s0/speed
+echo "duplex (good value full) : "
+cat /sys/class/net/enp2s0/duplex
+echo "errors :\n"
+cat /sys/class/net/enp2s0/statistics/rx_crc_errors
+cat /sys/class/net/enp2s0/statistics/rx_errors
 
 
